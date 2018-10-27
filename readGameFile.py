80bf6443e7b965fbb21aa17e2ee135483da989ae
Maxime Portaz <maxime.portaz@gmail.com>
2018-10-27 16:06:42 +0200

    add howlongtobeat

--- readGameFile.py
+++ readGameFile.py
@@ -1,41 +1,64 @@
 DEBUG=True
 
+from metacritic.metacritic import findCritic
+from howlongtobeat.howlongtobeat import findMainTimes
+
 def debugPrint(mess):
 	if DEBUG:
 		print(mess)
 
 class GameEntry:
-	def __init__(self, gameInfo, dict2Number):
+	def __init__(self, gameInfo, categories):
 		"""
 			GameInfo should be a list of note and info (like name)
-			dict2Number should give for each index of the list the corresponding category
+			dict2Number should give, for each index of the list, the corresponding category
 		"""
-		debugPrint("Creating entry with")
-		debugPrint(gameInfo)
-		debugPrint(len(dict2Number))
-		while len(gameInfo) < len(dict2Number):
-			gameInfo.append('0.0')
-		self.infos = {ind:gameInfo[dict2Number[ind]] for ind in dict2Number}
-	
+		
+		self.infos={}
+		
+		for i, cat in enumerate(categories):
+			if i < len(gameInfo):
+				self.infos[cat] = gameInfo[i]
+				
+					
+	def checkMissing(self, categories):
+		print("Checking data for title :", self.infos['Name'])
+		
+		if not 'metacritic score' in self.infos:
+			print("Looking for metacritic score online")
+			critics, _, _ = findCritic(self.infos['Name'])
+			self.infos['metacritic score'] = critics
+			
+		if not 'Temps de jeu' in self.infos:
+			print("Looking for howlongtobeat time")
+			mainTime = findMainTimes(self.infos['Name'])
+			self.infos['Temps de jeu'] = mainTime
+			
+			
+		
 	def __str__(self):
 		return str(self.infos)
 
 
 
-def gFile2Dict(fileName='Games.csv'):
+def gFile2Dict(fileName='Games.tsv', splitSym='\t'):
+	"""
+		Read fileName where the first line must be categories
+		each entry must be separated by splitSym
+	"""
 	lines = open(fileName).readlines()
-	categories = lines[0].split(',')
+	categories = lines[0].split(splitSym)
 
 	#check if metacritic score is already present
 	if not 'metacritic score' in categories:
 		categories.append('metacritic score')
-		dict2Number = {x:i for i,x in enumerate(categories)}
-	dict2Number = {x:i for i,x in enumerate(categories)}
 
 	#for each game, check every entry
 	for game in lines[1:]:
-		game = game.split(',')
-		gameEntry = GameEntry(game, dict2Number)
+		game = game.split(splitSym)
+		gameEntry = GameEntry(game, categories)
+		print(str(gameEntry))
+		gameEntry.checkMissing(categories)
 		print(str(gameEntry))
 		return
 
